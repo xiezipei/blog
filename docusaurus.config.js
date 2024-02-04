5a5a38188b67a96fe72acd69d21719ea110bb23d
xiezipei <zipeixie@gmail.com>
2024-02-04 16:56:28 +0800

    feat: 修改网站名称

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: `XZP's BLOG`,
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: `XZP's BLOG`,
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
