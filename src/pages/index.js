7cf48d5667832e7d73beb33dedc1330f65fa5348
xiezipei <zipeixie@gmail.com>
2024-02-06 16:54:07 +0800

    fix: 修复构建报错问题

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
