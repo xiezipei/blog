39bc6b4000c8ba87f6f69f953148f21e3bc78c26
xiezipei <zipeixie@gmail.com>
2024-02-18 12:48:33 +0800

    fix: 解决title解析问题

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: `XZP\'s BLOG`,
+  title: `XZP BLOG`,
   tagline: 'Build personal technical knowledge system.',
   favicon: 'img/favicon.ico',
 
@@ -64,7 +64,7 @@ const config = {
       // TODO: Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: `XZP's BLOG`,
+        title: `XZP`,
         logo: {
           alt: 'logo',
           src: 'img/logo.jpg',
